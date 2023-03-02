d2e89c99a3ce918c9b21217ad5ded41169f7002a
Kola Kolade <kolakolade@gmail.com>
2023-03-02 12:34:54 +0100

    Define Routes
    Install react-router-dom
    import BrowserRouter, Routes, Route and Link
    Define routes and links
    created Pages folder along home and quotes components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,23 @@
+import { Link, Route, Routes } from 'react-router-dom';
 import Calculator from './components/Calculator';
 
 function App() {
+  <>
+    <nav>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+          <Link to="/calculator">Calculator</Link>
+          <Link to="/quotes">Quotes</Link>
+        </li>
+      </ul>
+    </nav>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/calculator" element={<Calculator />} />
+      <Route path="/quotes" element={<Quotes />} />
+    </Routes>
+  </>;
   return (
     <div className="App">
       <Calculator initialValue={0} />
