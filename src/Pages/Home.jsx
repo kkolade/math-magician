6012190d243210736a62071953c0e4ca2b8cf5a5
Kola Kolade <kolakolade@gmail.com>
2023-03-02 13:21:36 +0100

    Home page content completed

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,7 +1,38 @@
 import React from 'react';
 
 function Home() {
-  return <div>Home</div>;
+  return (
+    <div>
+      <h1 className="headline">Welcome to Our Page!</h1>
+      <article className="article">
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat
+          delectus veritatis architecto ratione aspernatur dolores odit quaerat
+          voluptas, quam quis eveniet beatae ea repudiandae vero temporibus illo
+          facilis iure officia ipsa fugiat distinctio voluptates nulla vel?
+          Fugiat quo aliquam illum obcaecati quasi animi esse, repellat
+          doloremque sed et blanditiis ipsam ratione magnam expedita eum? Dolore
+          id minima voluptatum magni consequatur delectus minus molestias nobis
+          maiores. Numquam explicabo ad, pariatur quas ex sapiente sed repellat.
+          Ab quas consequuntur ipsum, cupiditate nisi nulla, mollitia incidunt
+          cumque cum ipsam corporis laudantium sequi architecto voluptatibus
+          quaerat! Eius expedita blanditiis nam fuga! Molestias, distinctio
+          provident?
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laborum
+          omnis dolor eveniet quas illum odio incidunt porro tenetur numquam
+          iusto atque culpa doloremque soluta, voluptatem illo iure saepe
+          delectus, id facilis suscipit et. Praesentium mollitia modi
+          exercitationem. Optio natus voluptatibus laudantium tempora esse,
+          facilis veniam incidunt nulla dolorem quia officiis dolor itaque aut
+          vitae doloribus nesciunt laborum quidem libero deleniti accusamus
+          dignissimos in. Aspernatur sint corporis iste molestias? Illo
+          provident alias, amet tempora aliquid reiciendis!
+        </p>
+      </article>
+    </div>
+  );
 }
 
 export default Home;
