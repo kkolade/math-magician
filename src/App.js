f7694120f5d6894af16864622cadd8e637a17502
Kola Kolade <kolakolade@gmail.com>
2023-03-02 12:45:05 +0100

    Routes working

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
 import { Link, Route, Routes } from 'react-router-dom';
 import Calculator from './components/Calculator';
+import Home from './Pages/Home';
+import Quotes from './Pages/Quotes';
 
 function App() {
-  <>
-    <nav>
-      <ul>
-        <li>
-          <Link to="/">Home</Link>
-          <Link to="/calculator">Calculator</Link>
-          <Link to="/quotes">Quotes</Link>
-        </li>
-      </ul>
-    </nav>
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/calculator" element={<Calculator />} />
-      <Route path="/quotes" element={<Quotes />} />
-    </Routes>
-  </>;
   return (
     <div className="App">
-      <Calculator initialValue={0} />
+      <>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+              <Link to="/calculator">Calculator</Link>
+              <Link to="/quotes">Quotes</Link>
+            </li>
+          </ul>
+        </nav>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/calculator" element={<Calculator />} />
+          <Route path="/quotes" element={<Quotes />} />
+        </Routes>
+      </>
+      {/* <Calculator initialValue={0} /> */}
     </div>
   );
 }
